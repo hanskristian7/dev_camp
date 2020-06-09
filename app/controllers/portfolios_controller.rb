ee9811ca2ab884c75105df973bb69904b1856b3f
hanskristian7 <hans.kristian1509@gmail.com>
2020-06-09 16:19:19 +0700

    added create portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,4 +2,22 @@ class PortfoliosController < ApplicationController
     def index
         @portfolios = Portfolio.all
     end
+
+    def new
+        @portfolio = Portfolio.new
+    end
+
+    def create
+        @portfolio = Portfolio.new(portfolio_params)
+        if @portfolio.save
+            redirect_to portfolios_path
+        else
+            render 'new'
+        end
+    end
+
+    private
+    def portfolio_params
+        params.require(:portfolio).permit(:title, :subtitle, :body)
+    end
 end
--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,2 +1,10 @@
 class Portfolio < ApplicationRecord
+    before_save :default_images
+
+    validates :title, presence: true
+
+    def default_images
+        self.main_image ||= "https://place-hold.it/600x400" if self.main_image.nil?
+        self.thumb_image ||= "https://place-hold.it/350x200" if self.thumb_image.nil?
+    end
 end
