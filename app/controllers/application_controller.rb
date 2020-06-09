e0859ca8952ec77e3d9c77ec9cd4482afd1996ba
hanskristian7 <hans.kristian1509@gmail.com>
2020-06-10 03:09:43 +0700

    trying to make gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,4 +14,17 @@ class ApplicationController < ActionController::Base
     #         email: "guest@example.com"
     #     )
     # end
+    before_action :set_copyright
+
+    def set_copyright
+        @copyright = HkViewTool::Renderer.copyright 'Hans Kristian', 'Learned from Jordan Hudgens'
+    end
+end
+
+module HkViewTool
+    class Renderer
+        def self.copyright name, msg
+            "&copy; #{Time.now.year} | <b>#{name} #{msg}</b?".html_safe
+        end
+    end
 end
