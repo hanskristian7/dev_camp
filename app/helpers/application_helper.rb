5431be325b6191c573dc495af82bed2bcf65bf5c
hanskristian7 <hans.kristian1509@gmail.com>
2020-06-10 01:08:33 +0700

    add view helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+    # def sample_helper
+    #     "<p>this is sample</p>".html_safe
+    # end
+
+    def nav_authentication_helper
+         if user_signed_in? 
+            (link_to 'Edit Profile', edit_user_registration_path) +
+            "<br>".html_safe +
+            (link_to 'Logout', destroy_user_session_path, method: :DELETE )
+        else 
+            (link_to 'Log in', new_user_session_path) + 
+            "<br>".html_safe +
+            (link_to 'Register', new_user_registration_path) 
+        end   
+    end
 end
