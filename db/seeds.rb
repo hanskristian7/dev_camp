2d8d81999838b4fe6e1e5e92c27db855e394a8c0
hanskristian7 <hans.kristian1509@gmail.com>
2020-06-09 15:39:47 +0700

    create seed file for sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |blog|
+    Blog.create!(
+        title: "My Blog Post #{blog+1}",
+        body: "Ruby on Rails, or Rails, is a server-side web application framework written in Ruby under the MIT License. Rails is a model–view–controller (MVC) framework, providing default structures for a database, a web service, and web pages. It encourages and facilitates the use of web standards such as JSON or XML for data transfer and HTML, CSS and JavaScript for user interfacing. In addition to MVC, Rails emphasizes the use of other well-known software engineering patterns and paradigms, including convention over configuration (CoC), don't repeat yourself (DRY), and the active record pattern.[4]"
+    )
+end
+puts "10 blog posts created"
+
+5.times do |skill|
+    Skill.create!(
+        title: "Rails #{skill}",
+        percent_utilized: 15
+    )
+end
+puts "5 skills created"
+
+9.times do |portfolio|
+    Portfolio.create!(
+        title: "Portfolio title: #{portfolio}",
+        subtitle: "My Portfolio",
+        body: "Ruby on Rails' emergence in 2005 greatly influenced web app development, through innovative features such as seamless database table creations, migrations, and scaffolding of views",
+        main_image: "https://place-hold.it/600x400",
+        thumb_image: "https://place-hold.it/350x200"
+    )
+end
+puts "9 skills created"
