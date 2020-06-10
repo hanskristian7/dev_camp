cacf7664ae34f659a6a14306349a6b2dde600662
hanskristian7 <hans.kristian1509@gmail.com>
2020-06-11 02:44:29 +0700

    add redis lib and comment generator

--- Gemfile
+++ Gemfile
@@ -53,6 +53,7 @@ gem 'dotenv-rails', '~> 2.1', '>= 2.1.2'
 gem 'cocoon', '~> 1.2', '>= 1.2.14'
 gem 'gritter', '~> 1.2'
 gem 'twitter', '~> 7.0'
+gem 'redis', '~> 4.2'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,12 +6,14 @@ class User < ApplicationRecord
   ## The multiple option can be set to true if you need users to have multiple roles.       ##
   petergate(roles: [:admin, :editor], multiple: false)                                      ##
   ############################################################################################ 
- 
+
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :comments, dependent: :destroy
 
   def first_name
     self.name.split.first
