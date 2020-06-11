c5d90b2599827d591d9207869a61b3cc9bcde29b
hanskristian7 <hans.kristian1509@gmail.com>
2020-06-11 07:06:30 +0700

    add comments form to blog

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,10 @@
 class CommentsController < ApplicationController
+    def create
+        @comment = Comment.new
+    end
+
+    private
+    def comment_param
+        params.require(:comment).permit(:content)
+    end
 end
